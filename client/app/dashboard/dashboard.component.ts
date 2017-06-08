0f75d79a7de441a25c13a069f571a8da49738a8f
Daniel Beyer <dan.beyer@yandex.com>
2017-06-08 11:42:38 +0200

    dashboard add

---
+++ client/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+
+import {dashboardHtml} from "./dashboard.html";
+
+@Component({
+    selector: "dashboard",
+    template: dashboardHtml
+})
+export class DashboardComponent {
+
+    constructor() {
+
+    }
+
+}
